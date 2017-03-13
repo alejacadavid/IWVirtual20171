c6ec0a26e9ae6225a81c0e1225d622ab930ccaeb
ALEJANDRA <ALEJANDRA@ALEJANDRA-PC>
2017-03-12 21:49:52 -0500

    Implementación con el patrón DAO

---
+++ JDBCEjemploClase0703/src/co/edu/udea/iw/dao/CiudadDAO.java
@@ -0,0 +1,26 @@
+package co.edu.udea.iw.dao;
+
+import java.util.List;
+import co.edu.udea.iw.exception.MyException;
+import co.edu.udea.iw.dto.Ciudad;
+
+/**
+ * Interface que me define los metodos permitidos para las Ciudades en el 
+ * sistema
+ * @author Maria Alejandra Cadavid Osorio - mariaa.cadavid@udea.edu.co
+ * @since Java 1.7
+ * @version 1.0
+ */
+
+public interface CiudadDAO {
+	
+	/**
+	 * Obtiene la lista de ciudades que existen en el sistema 
+	 * ordenadas alfabeticamente por el nombre
+	 * @return lista de ciudades ordenada
+	 * @throws MyException ocurre cuando hay un problema con la coneion a la BD
+	 */
+
+	public List<Ciudad> get() throws MyException;
+	
+}
